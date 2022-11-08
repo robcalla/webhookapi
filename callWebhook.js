4551d869de0562605e89c74e0845ef502d8d785b
Roberto Callari <robertocallari89@gmail.com>
2022-11-08 08:55:10 +0100

    up

--- callWebhook.js
+++ callWebhook.js
@@ -30,13 +30,13 @@ app.post('/test-api/webhook', async (req, res) => {
 
 async function callEvita(talk) {
 
-    const testUrl = "https://evita-test.opsilab.it/api/devices/send_data?deviceId=636249c7cbd19b5628c98deb&deviceToken=57fdde32-4e4f-4199-ba2f-34e3fe1c9991"
+    //const testUrl = "https://evita-test.opsilab.it/api/devices/send_data?deviceId=636249c7cbd19b5628c98deb&deviceToken=57fdde32-4e4f-4199-ba2f-34e3fe1c9991"
 
-    const evitaUrl = "https://evita-test.opsilab.it/api/devices/send_data?deviceId=636249c7cbd19b5628c98deb&deviceToken=57fdde32-4e4f-4199-ba2f-34e3fe1c9991"
+    const evitaUrl = "https://manager.evita.digital-enabler.eng.it/api/devices/send_data?deviceId=62cffa7ee50c9d08f994ec5f&deviceToken=4cd34f81-26cf-4b62-bf64-df9f251982db"
 
     const config = {
         method: 'post',
-        url: testUrl,
+        url: evitaUrl,
         headers: { 'Content-Type': 'application/json' },
         data:JSON.stringify({"message": talk})
     }
